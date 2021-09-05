4dd5f30c7743ea84595618b365f31f57d29eb35a
reihanalavi <reihanalavi@users.noreply.github.com>
2021-09-05 10:42:04 +0700

    first commit

--- app/src/main/java/com/akhza/anitahrdassistant/MainActivity.java
+++ app/src/main/java/com/akhza/anitahrdassistant/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //komen
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     }
