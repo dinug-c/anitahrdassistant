6f5a32e3e4df985224290068fd09cee4f38a5a3f
dinug-c <resmaadi@gmail.com>
2021-09-05 17:14:14 +0700

    entrance

---
+++ app/src/main/java/com/akhza/anitahrdassistant/activities/EntranceActivity.java
@@ -0,0 +1,17 @@
+package com.akhza.anitahrdassistant.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.akhza.anitahrdassistant.R;
+
+public class EntranceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrance);
+
+    }
+}
--- app/src/main/java/com/akhza/anitahrdassistant/activities/MainActivity.java
+++ app/src/main/java/com/akhza/anitahrdassistant/activities/MainActivity.java
@@ -2,8 +2,12 @@ package com.akhza.anitahrdassistant.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
 
+import com.akhza.anitahrdassistant.R;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,9 +15,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
         //komentar
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), EntranceActivity.class));
+                finish();
+            }
+        }, 2500L);
 
     }
 }
